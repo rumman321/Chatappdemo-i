4b792ab46da5354aff58e3729ac16eadb9df8809
rumman <cset-210652@dti.ac>
2023-10-23 11:22:41 +0600

    otp page desgin

--- app/src/main/java/com/example/otp/VerifificationActivity.kt
+++ app/src/main/java/com/example/otp/VerifificationActivity.kt
@@ -1,11 +1,22 @@
 package com.example.otp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.otp.databinding.ActivityVerifificationBinding
 
 class VerifificationActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verifification)
+
+       // startActivity(Intent(this,MainActivity::class.java))
     }
+
+
+
 }
