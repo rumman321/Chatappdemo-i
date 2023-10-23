be2241b62f40fc2457d1367f40ae73dbd3ce5d06
rumman <cset-210652@dti.ac>
2023-10-23 10:08:50 +0600

    ADD all dependencies ....

---
+++ app/src/main/java/com/example/otp/VerifificationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.otp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerifificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verifification)
+    }
+}
